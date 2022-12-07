5ad43b5d2ab404c3a79a81f8bb133cccc81d155d
alex-ahr <alexreaves@protonmail.com>
2022-12-07 15:38:30 -0500

    Updated 'lib.rs'

--- src/2022/src/lib.rs
+++ src/2022/src/lib.rs
@@ -1,6 +1,10 @@
 //! Shared functions.
 
+use std::env::current_exe;
 use std::fmt::Debug;
+
+#[cfg(feature = "speedtest")]
+use std::time::{Duration, Instant};
 
 pub trait Solution<ONE, TWO> {
     /// Returns the name of the day's puzzle.
@@ -29,4 +33,39 @@ pub trait Solution<ONE, TWO> {
     fn printspeed(time: &std::time::Duration) {
         println!("#time:\n{:?}", time);
     }
+}
+
+/// Prints day from name of program
+/// plus the name of the day's puzzle.
+pub fn printday(name: &str) {
+    println!(
+        "{} - {name} ///\n",
+        trim_path(current_exe().unwrap().to_str().unwrap())
+    );
+}
+
+/// Print each part, formatted correctly.
+pub fn printpart(partnum: i32, help: &str, answer: String) {
+    println!("#part{partnum}: {help}\n{answer}");
+}
+
+/// Prints how quickly each program executed.
+#[cfg(feature = "speedtest")]
+fn printtime(time: &Duration) {
+    println!("#time:\n{:?}", time);
+}
+
+/// Trims up to the last entry in path.
+///
+/// ex: `target/release/day3` to `day3`
+fn trim_path(path: &str) -> &str {
+    let len = path.split("/").collect::<Vec<&str>>().len();
+    path.split("/").collect::<Vec<&str>>()[len - 1]
+}
+
+/// Stops the timer a
+#[cfg(feature = "speedtest")]
+pub fn printtotaltime(time: Instant) {
+    let time = time.elapsed();
+    printtime(&time);
 }
